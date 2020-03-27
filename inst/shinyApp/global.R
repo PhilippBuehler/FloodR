b6a82e2648bb36fa02916a3cd7297e3bdc23490f
Philipp Bühler <31352013+PhilippBuehler@users.noreply.github.com>
2020-03-27 14:20:43 +0100

    Update global.R

--- inst/shinyApp/global.R
+++ inst/shinyApp/global.R
@@ -2,3 +2,4 @@ language <- FloodR::PKGENVIR$language
 Discharge <-FloodR::PKGENVIR$Discharge
 Precipitation <- FloodR::PKGENVIR$Precipitation
 Catchment_Properties <- FloodR::PKGENVIR$Catchment_Properties
+Dummy <- FloodR::PKGENVIR$Dummy
